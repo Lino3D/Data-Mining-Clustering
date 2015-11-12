99cd9c08529b44a7d67a4b41324ada7b2e9211fb
michcio91 <michcio91@gmail.com>
2015-11-12 13:40:22 +0100

    no message

---
+++ Data Mining/Data Mining/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+using System.IO;
+
+namespace Data_Mining
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void DocumentButton_Click(object sender, RoutedEventArgs e)
+        {
+     
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+            dlg.DefaultExt = ".txt";
+            dlg.Filter = "Text documents (.txt)|*.txt";
+
+            Nullable<bool> result = dlg.ShowDialog();
+
+            if (result == true)
+            {
+
+                string filename = dlg.FileName;
+                Paragraph paragraph = new Paragraph();
+                paragraph.Inlines.Add(System.IO.File.ReadAllText(filename));
+                FlowDocument document = new FlowDocument(paragraph);
+                FlowDocReader.Document = document;
+            } 
+        }
+    }
+}
